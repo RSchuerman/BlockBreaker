b9ab21a6b76eb941e5fae39efb03fc0d0f4c4bf7
Reed Schuerman <reed.schuerman@gmail.com>
2022-04-26 21:23:21 -0500

    Started initializing different blocks - routine commit

--- Program.cs
+++ Program.cs
@@ -19,7 +19,21 @@ namespace BlockBreaker
             Application.Run(new Form1());
 
             #region Initialize Blocks
+            //Blocks(BlockID, BreakTime, Tool, DropAmount, BreakLevel)
+            //BreakLevel - 0=fist 1=wood 2=stone 3=iron 4=diamond
+            //DropAmount - = amount or -1 if random in a range
             Blocks oakLog = new Blocks(1, 3, "Axe", 1, 0);
+            Blocks stone = new Blocks(2, 7.5, "Pickaxe", 1, 1);
+            Blocks dirt = new Blocks(3, 0.75, "Shovel", 1, 0);
+            Blocks ironOre = new Blocks(4, 15, "Pickaxe", 1, 2);
+            Blocks coalOre = new Blocks(5, 15, "Pickaxe", 1, 1);
+
+            List<Blocks> blocks = new List<Blocks>();
+            blocks.Add(oakLog);
+            blocks.Add(stone);
+            blocks.Add(dirt);
+            blocks.Add(ironOre);
+            blocks.Add(coalOre);
             #endregion
         }
     }
